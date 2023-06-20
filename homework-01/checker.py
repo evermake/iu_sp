78ad8ce6a3cf7935e47cc6e5904a88274acb5273
evermake <vladeryabkin@gmail.com>
2023-06-20 22:10:38 +0300

    hw(01): refactor `checker.py` script, add numbers count

--- homework-01/checker.py
+++ homework-01/checker.py
@@ -1,29 +1,28 @@
-import random
 import argparse
 
 maxint = 1 << 31
 
-parser = argparse.ArgumentParser(description = "Check that a file contains "\
-					       "not decreasing sequence of "\
-					       "numbers")
+parser = argparse.ArgumentParser(
+    description="Check that a file contains not decreasing sequence of numbers",
+)
 parser.add_argument('-f', type=str, required=True, help="file name")
 args = parser.parse_args()
 
-
-f = open(args.f, 'r')
-data = f.read()
-f.close()
+with open(args.f, 'r') as f:
+    data = f.read()
 
 data = data.split()
 prev_number = -(1 << 31 - 1)
+nums = 0
 for i in range(0, len(data)):
-	try:
-		v = int(data[i])
-		if v < prev_number:
-			print('Error on numbers {} {}'.format(prev_number, v))
-			exit(1)
-		prev_number = v
-	except:
-		pass
+    try:
+        v = int(data[i])
+        if v < prev_number:
+            print('Error on numbers {} {}'.format(prev_number, v))
+            exit(1)
+        prev_number = v
+        nums += 1
+    except Exception:
+        pass
 
-print('All is ok')
+print(f'All is ok ({nums} nums)')
